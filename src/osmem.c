254c21b1855596f275239a619243c31e6e8a82b3
marius.gaibu <marius.gaibu@stud.acs.upb.ro>
2024-11-03 16:24:33 +0200

    Refactor heap preallocation in separate function. Add insert function for memory blocks and primitive reuse functionality

--- src/osmem.c
+++ src/osmem.c
@@ -4,20 +4,50 @@
 #include <unistd.h>
 #include "osmem.h"
 #include "block_meta.h"
+#include "printf.h"
 
 #define ALIGNMENT 8
 #define ALIGN(size) (((size) + (ALIGNMENT - 1)) & ~(ALIGNMENT - 1))
 #define METADATA_SIZE	(sizeof(struct block_meta))
 #define MMAP_THRESHOLD (128 * 1024)
 
-static struct block_meta *block_metadata;
+static struct block_meta *block_metadata, *last_block;
+
+void insert_new_block(struct block_meta *meta, size_t size, int status,
+                      struct block_meta *prev, struct block_meta *next) {
+  set_block_metadata(meta, size, status, prev, next);
+  last_block->next = meta;
+  last_block = meta;
+}
 
 void set_block_metadata(struct block_meta *meta, size_t size, int status,
                         struct block_meta *prev, struct block_meta *next) {
   meta->size = size;
   meta->status = status;
-  meta->next = next;
   meta->prev = prev;
+  meta->next = next;
+}
+
+void preallocate_heap() {
+  void *heap = sbrk(MMAP_THRESHOLD);
+  if (heap == (void *)-1) {
+		DIE(heap == (void *)-1, "heap preallocation failed");
+		return NULL;
+	}
+
+  block_metadata = (struct block_meta *)heap;
+  set_block_metadata(block_metadata, MMAP_THRESHOLD - METADATA_SIZE, STATUS_FREE, NULL, NULL);
+  last_block = block_metadata;
+}
+
+struct block_meta *find_best_available_space(struct block_meta *meta, size_t payload_size) {
+  struct block_meta *best_block = NULL;
+  for (; meta != NULL; meta = meta->next) {
+    if (meta->status == STATUS_FREE && meta->size >= payload_size) {
+        best_block = meta;
+    }
+  }
+  return best_block;
 }
 
 void *os_malloc(size_t size)
@@ -26,35 +56,39 @@ void *os_malloc(size_t size)
     return NULL;
   }
 
-  size_t meta_size = ALIGN(size);
-  size_t block_size = meta_size + METADATA_SIZE;
+  size_t payload_size = ALIGN(size);
+  size_t block_size = ALIGN(size + METADATA_SIZE);
 
   if (block_size <= MMAP_THRESHOLD) {
-    void *new_block = (block_metadata == NULL ? sbrk(MMAP_THRESHOLD) : sbrk(block_size));
-
-    if (new_block == (void *)-1) {
-		  DIE(new_block == (void *)-1, "heap preallocation failed");
-		  return NULL;
-	  }
-
     if (block_metadata == NULL) {
-      block_metadata = (struct block_meta *)new_block;
-      set_block_metadata(block_metadata, MMAP_THRESHOLD - METADATA_SIZE, STATUS_ALLOC, NULL, NULL);
-    } else {
-      struct block_meta *tail = block_metadata;
-      while (tail->next) tail = tail->next;
-      set_block_metadata(new_block, meta_size, STATUS_ALLOC, tail, NULL);
+      preallocate_heap();
     }
 
+    struct block_meta *new_block = find_best_available_space(block_metadata, payload_size);
+
+    if (new_block) {
+      new_block->status = STATUS_ALLOC;
+    } else {
+      void *ptr = sbrk(block_size);
+      if (ptr == (void *)-1) {
+		    DIE(ptr == (void *)-1, "heap preallocation failed");
+		    return NULL;
+	    }
+
+      new_block = (struct block_meta *)ptr;
+      insert_new_block(new_block, payload_size, STATUS_ALLOC, last_block, NULL);
+    }
     return (char *)new_block + METADATA_SIZE;
   }
 
   void *new_block = mmap(NULL, block_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
+
   if (new_block == MAP_FAILED) {
     DIE(new_block == MAP_FAILED, "malloc mmap allocation failed");
     return NULL;
   }
-  set_block_metadata((struct block_meta*)new_block, meta_size, STATUS_MAPPED, NULL, NULL);
+
+  set_block_metadata((struct block_meta*)new_block, payload_size, STATUS_MAPPED, NULL, NULL);
   return (char *)new_block + METADATA_SIZE;
 }
 
@@ -65,7 +99,7 @@ void os_free(void *ptr)
   }
   struct block_meta *meta = (struct block_meta *)((char *)ptr - METADATA_SIZE);
   if (meta->status == STATUS_ALLOC) {
-    meta->status == STATUS_FREE;
+    meta->status = STATUS_FREE;
   }
   if (meta->status == STATUS_MAPPED) {
     munmap(meta, meta->size + METADATA_SIZE);
